6960141130726070922db51d9e3cf6c2ff4858ae
Christian Toribio <chris09toribio@gmail.com>
2025-03-21 15:07:02 -0700

    added link to github

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center bottom-0 py-4">
       <p className="text-sm text-center">
         Designed and developed by{" "}
-        <span className="font-bold">Christian Toribio</span>
+        <span className="font-bold">
+          <Link
+            target="_blank"
+            className="underline"
+            href={"https://github.com/tchrjs"}
+          >
+            @tchrjs
+          </Link>
+        </span>
       </p>
     </footer>
   );
