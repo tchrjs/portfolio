067ac9aed4b11438ef3bc395a9ac0dfc16082091
Christian Toribio <chris09toribio@gmail.com>
2025-04-07 17:35:09 -0700

    added in projects

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <></>;
+  return (
+    <div className="mt-[56px]">
+      <section className="container-wrapper">
+        <div className="container">
+          <div className="text-center py-8">work in progress...</div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,21 @@
-import Card from "@/components/ui/card";
+import {
+  Project,
+  ProjectContent,
+  ProjectDescription,
+  ProjectFooter,
+  ProjectTechnology,
+  ProjectTitle,
+} from "@/components/project/project";
+import {
+  Css3Original,
+  Html5Original,
+  NextjsOriginal,
+  ReactOriginal,
+  SupabaseOriginal,
+  TailwindcssOriginal,
+  TypescriptOriginal,
+} from "devicons-react";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -6,21 +23,70 @@ export default function Page() {
       <div className="container-wrapper">
         <div className="container">
           <div className="text-2xl py-8 text-center">2025</div>
-          <div className="flex flex-col gap-8 justify-center items-center">
-            <Card className="w-1/2">
-              <div className="flex">
-                <div className="p-2">
-                  <div className="text-2xl">Grocery Tracker</div>
-                  <div className="text-sm text-muted-foreground">
-                    A Progressive Web App to help track grocery prices and to
-                    help manage spendings.
-                  </div>
-                </div>
-              </div>
-            </Card>
-          </div>
         </div>
       </div>
+      <section className="container-wrapper">
+        <div className="container flex flex-col gap-8 md:w-3/4">
+          <Project>
+            <ProjectTitle>Grocery Tracker</ProjectTitle>
+            <ProjectDescription>
+              A <span className="text-[#04A777]">Progressive Web App</span> to
+              help track grocery prices and to help manage spendings.
+            </ProjectDescription>
+            <ProjectContent>
+              <div className="flex items-center justify-center w-[240px] h-[488px] contain-content">
+                <img
+                  className="absolute object-contain size-[465px] rounded-4xl"
+                  src="/grocery-tracker/view-only.gif"
+                ></img>
+                <img
+                  className="absolute object-contain"
+                  src="/iphone16-template.png"
+                />
+              </div>
+            </ProjectContent>
+            <ProjectFooter>
+              <ProjectTechnology>
+                <NextjsOriginal size={30} />
+                <ReactOriginal size={30} />
+                <TypescriptOriginal size={30} />
+                <TailwindcssOriginal size={30} />
+                <Html5Original size={30} />
+                <Css3Original size={30} />
+                <SupabaseOriginal size={30} />
+              </ProjectTechnology>
+              <div className="text-muted text-center text-nowrap mt-4">
+                <Link href="https://tchrjs-grocery-tracker.vercel.app/">
+                  tchrjs-grocery-tracker.vercel.app
+                </Link>
+              </div>
+            </ProjectFooter>
+          </Project>
+          <Project>
+            <ProjectTitle>My Portfolio</ProjectTitle>
+            <ProjectDescription>
+              as My personal website to showcase showcase projects, who I am,
+              and services I can provide.
+            </ProjectDescription>
+            <ProjectContent></ProjectContent>
+            <ProjectFooter>
+              <ProjectTechnology>
+                <NextjsOriginal size={30} />
+                <ReactOriginal size={30} />
+                <TypescriptOriginal size={30} />
+                <TailwindcssOriginal size={30} />
+                <Html5Original size={30} />
+                <Css3Original size={30} />
+              </ProjectTechnology>
+              <div className="text-muted text-center text-nowrap mt-4">
+                <Link href="https://tchrjs-portfolio.vercel.app/">
+                  tchrjs-portfolio.vercel.app
+                </Link>
+              </div>
+            </ProjectFooter>
+          </Project>
+        </div>
+      </section>
     </div>
   );
 }
