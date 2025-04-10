aee0a3d5bbb05f62cf00a468c4a7e93ac3eda286
Christian Toribio <chris09toribio@gmail.com>
2025-04-10 15:30:19 -0700

    remove tech section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/home/hero";
 import PageContent from "@/components/layout/page-content";
-import Tech from "@/components/home/tech";
 import About from "@/components/home/about";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <PageContent>
       <Hero />
       <About />
-      <Tech />
     </PageContent>
   );
 }
