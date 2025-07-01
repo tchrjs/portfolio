9ecbad68e01863cfb12cc4c0cf37cd2265f9f531
Christian Toribio <chris09toribio@gmail.com>
2025-07-01 14:13:49 -0700

    simple project page setup

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,15 @@
+import Projects from "@/markdown/projects.mdx";
+import Footer from "@/components/footer";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div>
+      <main className="px-7 py-10">
+        <article className="prose dark:prose-invert m-auto">
+          <Projects />
+        </article>
+        <Footer />
+      </main>
+    </div>
+  );
 }
