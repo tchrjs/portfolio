9f255eee45624aa1c1231e6d22e03beb940fd331
Christian Toribio <chris09toribio@gmail.com>
2025-11-10 14:11:47 -0800

    added more projects

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
 export const SITE_TITLE = "Christian Toribio | Developer Portfolio";
 export const SITE_DESCRIPTION =
     "A passionate front-end and game developer focused on clean code and seamless user experiences";
+
+export const BLOG_TITLE = "Christian Toribio | Blog Posts";
+export const BLOG_DESCRIPTION =
+    "A space where I document my growth as a developer, share experiences, and discuss ideas.";
