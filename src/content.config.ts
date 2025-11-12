4eedac7a94431a7705ed228f3141819fb0c58f82
Christian Toribio <chris09toribio@gmail.com>
2025-11-12 11:35:34 -0800

    setup for first blog post

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,7 +8,6 @@ const blog = defineCollection({
     schema: () =>
         z.object({
             title: z.string(),
-            description: z.string(),
             pubDate: z.coerce.date(),
             updatedDate: z.coerce.date().optional(),
         }),
